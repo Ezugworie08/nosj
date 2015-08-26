e1b87392311981307193eacee5834534e47e54fd
ezugworie I.e <ezugworie.ikechukwu@live.com>
2015-08-26 15:15:22 -0500

    Removed unnecessary comments.

--- app/forms.py
+++ app/forms.py
@@ -5,12 +5,6 @@ from django import forms
 from app.models import JSONFile
 
 
-# def verify_json(file):
-#     with open(file) as f:
-#         print("print pussy")
-#         data = json.loads(f.read())
-
-
 class JSONUploadForm(forms.ModelForm):
     class Meta:
         model = JSONFile
